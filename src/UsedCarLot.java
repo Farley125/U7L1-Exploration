82e3e1aa5868cd74fd8497ad9810385b963de7c1
BT_4N2_30 <fasd>
2023-02-01 12:12:13 -0500

    Added 4 methods to the UsedCarLot class.

--- src/UsedCarLot.java
+++ src/UsedCarLot.java
@@ -35,4 +35,65 @@ public class UsedCarLot
             inventory.set(idx2, inventory.set(idx1, inventory.get(idx2)));
         }
     }
+
+    /* Adds a Car to the the inventory list at the index specified
+    by indexToAdd; this method increases the size of inventory by 1
+
+    PRECONDITION: indexToAdd < inventory.size()
+  */
+    public void addCar(int indexToAdd, Car carToAdd)
+    {
+        inventory.add(indexToAdd, carToAdd);
+    }
+
+    /* "sells" the Car located at indexOfCarToSell which
+        removes it from the inventory list and shifting the remaining
+        Cars in the inventory list to the left to fill in the gap;
+        this method reduces the size of inventory by 1
+
+        @return returns the Car that is being "sold" (removed from lot)
+
+        PRECONDITION: indexOfCarToSell < inventory.size()
+
+        HINT: the ArrayList remove() method returns the removed element...
+      */
+    public Car sellCarShift(int indexOfCarToSell)
+    {
+        return inventory.remove(indexOfCarToSell);
+    }
+
+    /* "sells" the Car located at indexOfCarToSell,
+       but instead of removing it and shifting the inventory
+       list to the left, REPLACE the Car at indexOfCarToSell
+       with NULL, thus creating an "empty parking spot" on the lot;
+       this method does NOT reduce the size of inventory by 1
+
+       PRECONDITION: indexOfCarToSell < inventory.size()
+
+       @return returns the Car that is being "sold" (replaced with null)
+     */
+    public Car sellCarNoShift(int indexOfCarToSell)
+    {
+        return inventory.set(indexOfCarToSell, null);
+    }
+
+    /* moves Car located at index indexOfCarToMove to index destinationIndex;
+       if destinationIndex > indexOfCarToMove, moves the Car to the right in
+       inventory list; if destinationIndex < indexOfCarToMove, moves the
+       Car to the left in inventory list
+
+       PRECONDITIONS: indexOfCarToMove < inventory.size()
+                      destinationIndex < inventory.size()
+     */
+    public void moveCar(int indexOfCarToMove, int destinationIndex)
+    {
+        if (destinationIndex > indexOfCarToMove) {
+            inventory.add(destinationIndex+1, inventory.get(indexOfCarToMove));
+            inventory.remove(indexOfCarToMove);
+        } else {
+            inventory.add(destinationIndex, inventory.get(indexOfCarToMove));
+            inventory.remove(indexOfCarToMove+1);
+        }
+    }
+
 }
